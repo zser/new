dbced30707472cd66fa6acaf5c7e9afd2ee613fd
zhangshuai <1591184273@qq.com>
2017-11-15 09:41:17 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -4,4 +4,4 @@ var http = require("http");
      response.write("Hello World!!"); 
      response.end(); 
  }).listen(8000); 
- console.log('Server running at http://127.0.0.1:8000/');
+ console.log('hello world');
